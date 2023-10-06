1106844603c68b0103521d1cc20f6e1c4d7b9e18
pmh5574 <g_a_m_e_@naver.com>
2023-10-06 22:49:00 +0900

    231006 뭔가 안되네..

--- src/main/java/com/calendar/workout/domain/posts/PostsRepository.java
+++ src/main/java/com/calendar/workout/domain/posts/PostsRepository.java
@@ -3,5 +3,6 @@ package com.calendar.workout.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 }
--- src/main/java/com/calendar/workout/web/posts/PostsController.java
+++ src/main/java/com/calendar/workout/web/posts/PostsController.java
@@ -1,21 +1,28 @@
 package com.calendar.workout.web.posts;
 
+import com.calendar.workout.domain.posts.Posts;
+import com.calendar.workout.domain.posts.PostsRepository;
 import com.calendar.workout.dto.posts.request.PostsSaveRequestDto;
 import com.calendar.workout.service.posts.PostsService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 public class PostsController {
 
     private final PostsService postsService;
+    private final PostsRepository postsRepository;
 
     @PostMapping("/posts")
     public void savePosts(@RequestBody @Validated PostsSaveRequestDto postsSaveRequestDto) {
         postsService.save(postsSaveRequestDto);
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Posts get(@PathVariable Long postId) {
+        return postsRepository.getById(postId);
     }
 }
