91b63b4394243475cb529617f4179d665028376f
pmh5574 <g_a_m_e_@naver.com>
2023-10-26 15:53:05 +0900

    #!/bin/bash
    firstLine=`head -2 $1 | tail -1`
    if [[ $firstLine == \#* ]]; then # Testing that the file starts with a comment, not yet a real commit ;)
      echo '<type>(<component>): <subject>' > .prepare-commit-msg-temp
      echo '' >> .prepare-commit-msg-temp
      echo '<body>' >> .prepare-commit-msg-temp
      echo '' >> .prepare-commit-msg-temp
      echo '# types: feat, fix, docs, style, refactor, test, chore(mantean)' >> .prepare-commit-msg-temp
    
      { cat .prepare-commit-msg-temp; cat $1; } > .prepare-commit-msg-temp2
      cat .prepare-commit-msg-temp2 > $1
      rm .prepare-commit-msg-temp .prepare-commit-msg-temp2
    fi

---
+++ src/test/java/com/calendar/workout/web/auth/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package com.calendar.workout.web.auth;
+
+import com.calendar.workout.dto.auth.AuthTokens;
+import com.calendar.workout.dto.auth.request.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+
+@WebMvcTest(AuthController.class)
+@MockBean(JpaMetamodelMappingContext.class)
+@AutoConfigureRestDocs
+class AuthControllerTest {
+
+    @Autowired
+    private AuthController authController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final static String REFRESH_TOKEN = "refreshToken";
+    private final static String ACCESS_TOKEN = "accessToken";
+
+    @DisplayName("로그인")
+    @Test
+    void login() {
+        // given
+        LoginRequest loginRequest = new LoginRequest("code");
+        AuthTokens authTokens = AuthTokens.builder()
+                .accessToken(ACCESS_TOKEN)
+                .refreshToken(REFRESH_TOKEN)
+                .build();
+
+        // when
+
+        // then
+    }
+
+    void test() {
+
+    }
+}
