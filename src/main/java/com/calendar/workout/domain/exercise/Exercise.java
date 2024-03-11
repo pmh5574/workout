6364f35e909187f9c74d86fadf1c4469cd104352
pmh5574 <g_a_m_e_@naver.com>
2024-03-12 01:21:44 +0900

    fix: 운동 변경

--- src/main/java/com/calendar/workout/domain/exercise/Exercise.java
+++ src/main/java/com/calendar/workout/domain/exercise/Exercise.java
@@ -1,18 +1,24 @@
 package com.calendar.workout.domain.exercise;
 
-import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import static lombok.AccessLevel.PROTECTED;
 
+import com.calendar.workout.domain.BaseTimeEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
-
-import static lombok.AccessLevel.PROTECTED;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Entity
-public class Exercise {
+public class Exercise extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +29,6 @@ public class Exercise {
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
-    private List<CategoryExercise> categoryList = new ArrayList<>();
+    private List<CategoryExercise> categories = new ArrayList<>();
 
 }
